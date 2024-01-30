7a9c47d1e54f3ffc5fdd9fd44fdc7be4eb3897c6
Daniil Grebnev <danilgrebneff@yandex.ru>
2024-01-30 14:56:38 +0300

    moment убран из зависимостей

--- src/features/functions/weekInit.ts
+++ src/features/functions/weekInit.ts
@@ -1,17 +1,19 @@
-import moment from 'moment'
-import 'moment/locale/ru'
-
-const weekNum = Number(moment().format('w'))
-let parityRu = weekNum % 2 !== 0 ? 'Числитель' : 'Знаменатель'
-let parityEng = weekNum % 2 !== 0 ? 'numerator' : 'denominator'
+const currentDate = new Date()
+const weekNum = Math.ceil(
+	(currentDate.getTime() -
+		new Date(currentDate.getFullYear(), 0, 1).getTime()) /
+		(7 * 24 * 60 * 60 * 1000)
+)
+let parityRu: string = weekNum % 2 !== 0 ? 'Числитель' : 'Знаменатель'
+let parityEng: string = weekNum % 2 !== 0 ? 'numerator' : 'denominator'
 
 let dayOfWeek: string
 
-const dayNum = moment().day()
+const dayNum: number = currentDate.getDay()
 console.log(dayNum)
-if (dayNum == 0) {
+if (dayNum === 0) {
 	dayOfWeek = 'понедельник'
-	if (parityEng == 'denominator') {
+	if (parityEng === 'denominator') {
 		parityEng = 'numerator'
 		parityRu = 'Числитель'
 	} else {
@@ -19,12 +21,31 @@ if (dayNum == 0) {
 		parityRu = 'Знаменатель'
 	}
 } else {
-	dayOfWeek = moment().locale('ru').format('dddd')
+	const ruDaysOfWeek: string[] = [
+		'воскресенье',
+		'понедельник',
+		'вторник',
+		'среда',
+		'четверг',
+		'пятница',
+		'суббота',
+	]
+	dayOfWeek = ruDaysOfWeek[dayNum]
 }
 
-const day = moment().format('LL')
+const options: Intl.DateTimeFormatOptions = {
+	year: 'numeric',
+	month: 'long',
+	day: 'numeric',
+}
+const day: string = currentDate.toLocaleDateString('ru-RU', options)
 
-const weekData = {
+const weekData: {
+	parityEng: string
+	parityRu: string
+	dayOfWeek: string
+	day: string
+} = {
 	parityEng,
 	parityRu,
 	dayOfWeek,
